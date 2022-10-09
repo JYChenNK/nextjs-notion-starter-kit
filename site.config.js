4fd14bbc3e1eca0ce26adad56a2a1045ec203deb
JYChen <30278462+JYChenNK@users.noreply.github.com>
2022-10-09 11:27:47 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: '萤火',
-  domain: 'https://cat-move-b38.notion.site/Blog-4057e2d245834ed9b424bb4eed5707f6',
+  domain: 'https://www.notion.so/Blog-4057e2d245834ed9b424bb4eed5707f6',
   author: 'Jayren Chen',
 
   // open graph metadata (optional)
